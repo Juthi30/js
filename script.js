30a551a2817c7012906fcfa470c99b463627657a
Juthi30 <juthi2678@gmail.com>
2024-12-31 13:33:55 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,118 @@
+// console.log('hello world')
+
+// let sum=9*9
+// console.log(sum)
+
+// document.getElementById('hello').
+// innerHTML=sum
+
+// if(sum < 50){
+//     document.getElementById('hello').innerHTML="right"
+// }
+// else{
+//       document.getElementById('hello').innerHTML="wrong"
+// }
+
+
+
+
+// function 
+
+// function pk(name){
+//     console.log(`iuyth ${name}`);
+// }
+// pk("green")
+
+// // variable 
+
+// var name = ("bithi");
+
+// console .log(name)
+
+// var Flower =("juthi");
+
+// console .log(Flower)
+
+
+
+// // condition  
+
+// if(50>=70){
+//     console .log("Yes")
+// }else{
+//     console .log("No")
+// }
+
+//     const salary = 20000
+//     const height = 6
+//     const bcs = true
+
+//     if(salary > 25000||(bcs == true && height>5)){
+//         console .log("yes yes yes")
+//     }else{
+//         console .log("no no no")
+//     }
+
+//     const sa=30000
+//     const he=6
+
+//     if(sa<25000 && he>5){
+//         console .log("yes");
+//     }else{
+//         console .log("no");
+//     }
+
+
+//     const price =1500
+     
+//     if(1500 <= price){
+//         const discount = price * 10 / 100;
+//         const pay = price - discount;
+//         console .log(pay);
+//     }
+//     else if(price < 1500){
+//         const discount = price * 5 / 100;
+//         const pay = price - discount;
+//         console .log(pay);
+//     }
+    
+//     else{
+//         console .log(price);
+//     }
+
+
+
+//     const age = 20 
+
+//     age>=20? console .log("get discount" ): console .log( "pay full")
+
+
+//     let addToCart = 100
+//     const issBoss = false
+
+//     addToCart = issBoss === true? 0 : addToCart + 100;
+//     console .log(addToCart)
+
+
+
+let age1 = 60 
+const  hasNid = true
+ const  child = 6; 
+const hasCoupon = true
+ 
+ if( child<=5){
+    console . log ("free")
+ }
+ else if( age1>=70 && hasNid==true){
+    console.log("50% discount")
+ }
+  else if  ( hasCoupon ==true){
+    console .log ("25% discount")
+ }
+ else{
+    console. log("no no")
+ }
+
+
+
+
